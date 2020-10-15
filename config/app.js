c1b8f4449737a69f59f6d3e8e88982159e6acc80
fernandes-dev <eduardo.yugan@gmail.com>
2020-10-15 16:31:39 -0400

    using proxy

--- config/app.js
+++ config/app.js
@@ -52,7 +52,7 @@ module.exports = {
     | values. Read documentation for that.
     |
     */
-    trustProxy: false,
+    trustProxy: true,
 
     /*
     |--------------------------------------------------------------------------
