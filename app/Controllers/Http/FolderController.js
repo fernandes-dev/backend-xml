54ff06e9a4843b4c8a8d8953a042a39dc9129b82
fernandes-dev <eduardo.yugan@gmail.com>
2020-11-04 17:26:46 -0400

    test

--- app/Controllers/Http/FolderController.js
+++ app/Controllers/Http/FolderController.js
@@ -124,7 +124,7 @@ class FolderController {
 
     try {
       if (isZip || type === 'zip') {
-        return response.attachment(resolved);
+        return response.send(resolved);
       }
 
       if (type === 'directory') {
@@ -158,7 +158,7 @@ class FolderController {
         zipFile.addLocalFile(resolved);
         zipFile.writeZip(`${newDir}${typeBar}${name}.zip`);
 
-        return response.attachment(`${newDir}${typeBar}${name}.zip`);
+        return response.send(`${newDir}${typeBar}${name}.zip`);
       }
     } catch (error) {
       return response
